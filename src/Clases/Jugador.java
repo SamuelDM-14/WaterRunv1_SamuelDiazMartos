ca4045d4c9555475a6c3f674306d30e6f294b493
Samuel Diaz <samidm23@gmail.com>
2025-03-07 13:02:45 +0100

    COmento jugador y termino jugar

--- src/Clases/Jugador.java
+++ src/Clases/Jugador.java
@@ -1,37 +1,46 @@
+/**
+ * Jugador
+ * @author SDM
+ * 07-03-2025
+ */
 package clases;
+/**
+ * Clase Jugador.
+ * Esta clase se encarga de gestionar la información del jugador.
+ */
 public class Jugador {
-
+    //Atributos del jugador
     private String fechaInicio = "";
     private String nombre = "";
 
-    
+    //Metodo constructor de jugador
     public Jugador(String fechaInicio, String nombre) {
         this.fechaInicio = fechaInicio;
         this.nombre = nombre;
     }
 
-
+    //Metodo para ver la fecha de inicio.
     public String getFechaInicio() {
         return fechaInicio;
     }
 
-
+    //Metodo para modificar la fecha de inicio
     public void setFechaInicio(String fechaInicio) {
         this.fechaInicio = fechaInicio;
     }
 
-
+    //Metodo para ver el nombre del jugador.
     public String getNombre() {
         return nombre;
     }
 
-
+    //Metodo para modificar el nombre del jugador.
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
 
-    @Override
+    @Override //Metodo toString para mostrar el contenido del Jugador.
     public String toString() {
         return "Jugador [fechaInicio=" + fechaInicio + ", nombre=" + nombre + "]";
     }
