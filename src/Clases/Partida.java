1b07c7c1a1a597e43f4cab56744f9918a8883794
Samuel Diaz <samidm23@gmail.com>
2025-03-12 09:20:00 +0100

    Inicio de la creacion de la clase partida

---
+++ src/Clases/Partida.java
@@ -0,0 +1,14 @@
+/**
+ * Partida
+ * @author SDM
+ * 14-03-2025
+ */
+package clases;
+
+/**
+ * La clase partida se encarga de gestionar la hora de inicio, la hora de fin
+ * las respuestas correctas y si ha pasado el nivel o no el jugador.
+ */
+public class Partida {
+    
+}
