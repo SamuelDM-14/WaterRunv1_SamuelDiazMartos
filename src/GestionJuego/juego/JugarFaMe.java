dc0ca215cc55866745187dec4a06c3bb3499f4fd
Samuel Diaz <samidm23@gmail.com>
2025-03-16 23:07:19 +0100

    Termino el JugarFaMe, empiezo las pruebas de funcionam,iento

--- src/GestionJuego/juego/JugarFaMe.java
+++ src/GestionJuego/juego/JugarFaMe.java
@@ -9,7 +9,7 @@ import utilidades.VarGenYConst;
 
 public class JugarFaMe {
 
-        /**
+    /**
      * Metodo que gestiona la partida de dificultad Facil y Medio
      * 
      * @param dificultad Dificultad de la partida
@@ -17,166 +17,122 @@ public class JugarFaMe {
      * @throws IOException
      */
     public static void jugarFaMe() throws IOException {
+
         Boolean lvlPasado = false; // Variable que comprueba si te has pasdo un nivel.
-        VarGenYConst.min = 1;
-        VarGenYConst.max = 4;
-        
+
         int reAcertadas = 0;
         int enunciado = 0;
-        int reJugador;
-        String[] opciones;
         String respuestaElegida;
+        int msglvl = 0;
+        boolean acertada = false;
         GestionPreguntas gp = new GestionPreguntas();
         Pregunta p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
         String[] pantallas = Utilidades.gestorPantallas();
         int mostrarPantalla = 0;
         String pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
 
-        opciones = p.getOpciones();
         do {
             reAcertadas = 0;
             pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-            System.out.println(pantalla);
-            System.out.println(p.getEnunciado());
-            for (int i = 0; i < opciones.length; i++) {
-                if (!opciones[i].isEmpty()) {
-                    System.out.println((i + 1) + ") " + opciones[i]);
-                }
-            }
-            reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-            respuestaElegida = opciones[reJugador - 1];
+            Utilidades.mostrarPregunta(p, pantalla);
+            respuestaElegida = Utilidades.leerRespuesta(p);
+
             if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                reAcertadas = reAcertadas + 1;
+                reAcertadas++;
                 enunciado++;
+                msglvl = 0;
                 p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                opciones = p.getOpciones();
-                System.out.println("¡Has acertado! Tu personaje avanza hasta la rendija de salida.");
+
+                System.out.println(VarGenYConst.mensajesJuego[msglvl]);
+
                 mostrarPantalla++;
                 pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                System.out.println(pantalla);
-                System.out.println(p.getEnunciado());
-                for (int i = 0; i < opciones.length; i++) {
-                    if (!opciones[i].isEmpty()) {
-                        System.out.println((i + 1) + ") " + opciones[i]);
-                    }
-                }
-                reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                respuestaElegida = opciones[reJugador - 1];
+
+                Utilidades.mostrarPregunta(p, pantalla);
+                respuestaElegida = Utilidades.leerRespuesta(p);
+
                 if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                    reAcertadas = reAcertadas + 1;
+                    reAcertadas++;
                     enunciado++;
+                    msglvl = 1;
                     p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                    opciones = p.getOpciones();
-                    System.out.println("¡Has acertado! Tu personaje empieza a abrir la cerradura.");
+
+                    System.out.println(VarGenYConst.mensajesJuego[msglvl]);
                     pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                    System.out.println(pantalla);
-                    System.out.println(p.getEnunciado());
-                    for (int i = 0; i < opciones.length; i++) {
-                        if (!opciones[i].isEmpty()) {
-                            System.out.println((i + 1) + ") " + opciones[i]);
-                        }
-                    }
-                    reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                    respuestaElegida = opciones[reJugador - 1];
+                    Utilidades.mostrarPregunta(p, pantalla);
+                    respuestaElegida = Utilidades.leerRespuesta(p);
                     if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                        p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                        System.out.println("¡Has acertado! Tu personaje ha huido.");
+                        acertada = true;
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
                         lvlPasado = true; // pone lvlPasado en true para salir del bucle.
-                        VarGenYConst.salirJuego = true; // Te manda al menú princial.
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     } else {
-                        System.out.println("Has fallado.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // Te saca del juego tras perder
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     }
                 } else {
                     enunciado++;
+                    msglvl = 2;
                     p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                    opciones = p.getOpciones();
-                    System.out.println("Has fallado.");
+
+                    System.out.println(VarGenYConst.mensajesJuego[msglvl]);
                     pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                    System.out.println(pantalla);
-                    System.out.println(p.getEnunciado());
-                    for (int i = 0; i < opciones.length; i++) {
-                        if (!opciones[i].isEmpty()) {
-                            System.out.println((i + 1) + ") " + opciones[i]);
-                        }
-                    }
-                    reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                    respuestaElegida = opciones[reJugador - 1];
+                    Utilidades.mostrarPregunta(p, pantalla);
+                    respuestaElegida = Utilidades.leerRespuesta(p);
                     if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                        System.out.println("¡Has acertado! Tu personaje empieza a abir la cerradura.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true;
+                        acertada = true;
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     } else {
-                        System.out.println("Has fallado.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // Te saca del juego tras perder
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     }
                 }
             } else {
                 enunciado++;
+                msglvl = 2;
                 p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                opciones = p.getOpciones();
-                System.out.println("Has fallado.");
+
+                System.out.println(VarGenYConst.mensajesJuego[msglvl]);
                 pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                System.out.println(pantalla);
-                System.out.println(p.getEnunciado());
-                for (int i = 0; i < opciones.length; i++) {
-                    if (!opciones[i].isEmpty()) {
-                        System.out.println((i + 1) + ") " + opciones[i]);
-                    }
-                }
-                reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                respuestaElegida = opciones[reJugador - 1];
+                Utilidades.mostrarPregunta(p, pantalla);
+                respuestaElegida = Utilidades.leerRespuesta(p);
                 if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                    reAcertadas = reAcertadas + 1;
+                    reAcertadas++;
                     enunciado++;
+                    msglvl = 0;
                     p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                    opciones = p.getOpciones();
-                    System.out.println("¡Has acertado! Tu personaje avanza hasta la rendija de salida.");
+
+                    System.out.println(VarGenYConst.mensajesJuego[msglvl]);
                     mostrarPantalla++;
                     pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                    System.out.println(pantalla);
-                    System.out.println(p.getEnunciado());
-                    for (int i = 0; i < opciones.length; i++) {
-                        if (!opciones[i].isEmpty()) {
-                            System.out.println((i + 1) + ") " + opciones[i]);
-                        }
-                    }
-                    reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                    respuestaElegida = opciones[reJugador - 1];
+                    Utilidades.mostrarPregunta(p, pantalla);
+                    respuestaElegida = Utilidades.leerRespuesta(p);
                     if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                        System.out.println("¡Has acertado! Tu personaje empieza a abir la cerradura.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // te saca del juego tras perder
+                        acertada = true;
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     } else {
-                        System.out.println("Has fallado.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // Te saca del juego tras perder
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     }
                 } else {
                     enunciado++;
                     p = gp.getPregunta(VarGenYConst.dificultad, enunciado);
-                    opciones = p.getOpciones();
-                    System.out.println("Has fallado.");
+                    msglvl = 2;
+                    System.out.println(VarGenYConst.mensajesJuego[msglvl]);
                     pantalla = pantallas[mostrarPantalla]; // Mas adelante, incluido el color del personaje y del agua
-                    System.out.println(pantalla);
-                    System.out.println(p.getEnunciado());
-                    for (int i = 0; i < opciones.length; i++) {
-                        if (!opciones[i].isEmpty()) {
-                            System.out.println((i + 1) + ") " + opciones[i]);
-                        }
-                    }
-                    reJugador = Utilidades.leerEnteroValidado(VarGenYConst.min, VarGenYConst.max); // Comprueba la respuesta 1
-                    respuestaElegida = opciones[reJugador - 1];
+
+                    Utilidades.mostrarPregunta(p, pantalla);
+                    respuestaElegida = Utilidades.leerRespuesta(p);
+
                     if (respuestaElegida.equals(p.getRespuestaCorrecta())) {
-                        System.out.println("¡Has acertado! Tu personaje empieza a abir la cerradura.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // Te saca del juego tras perder
+                        acertada = true;
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     } else {
-                        System.out.println("Has fallado.");
-                        System.out.println("Oh no, el agua subió demasiado rápido, has perdido.");
-                        VarGenYConst.salirJuego = true; // Te saca del juego tras perder
+                        Utilidades.finalizarJuegoFaMe(acertada, reAcertadas, msglvl);
+                        Utilidades.guardarPartida(reAcertadas, lvlPasado);
                     }
 
                 }
